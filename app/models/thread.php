453e41da11bddc1d6fdc1f8964bacbdb97ea6af6
Ezra Finones <e.finones@klab.com>
2015-03-11 16:11:58 +0800

    Create and modify files for comment writing

--- app/models/thread.php
+++ app/models/thread.php
@@ -35,6 +35,13 @@ class Thread extends AppModel
            $comments[] = new Comment($row);
         }        
         return $comments;
-    }                    
+    }    
+    public function write(Comment $comment)                    
+    {
+        $db = DB::conn();
+        $db->query('INSERT INTO comment 
+                SET thread_id = ?, username = ?, body = ?, created = NOW()', 
+                array($this->id, $comment->username, $comment->body));                    
+    }         
 }
 
